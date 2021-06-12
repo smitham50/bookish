3edc17bc87613bd17ee5d73ecbee4f3cefad6400
Aaron Smith <smitham50@gmail.com>
2021-06-12 14:32:54 -0400

    set up jest and run sample test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import React, { useEffect } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import axios from "axios";
 
 export default function Home(props) {
