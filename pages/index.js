e5ee5aa8d0d9c2456dabaf92ee08b0aebfa9776f
Aaron Smith <smitham50@gmail.com>
2021-06-13 10:41:08 -0400

    build simple search form that renders images of results

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,78 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import styled from 'styled-components';
+
+const Image = styled.img`
+  margin: 1rem;
+`;
+
+function joinWords(keywords) {
+  return keywords.split(' ').join('+');
+}
 
 export default function Home(props) {
+  const [searchTerms, setSearchTerms] = useState('');
+  const [searchAuthor, setSearchAuthor] = useState('');
+  const [books, setBooks] = useState(null);
+
+  const renderResults = () => {
+    const validBooks = books.filter(book => book.volumeInfo.imageLinks)
+
+    return validBooks.map((book, i) => {
+      return <Image key={i} src={book.volumeInfo.imageLinks.smallThumbnail} alt=""/>
+    });
+  }
+
   useEffect(() => {
-    console.log('in effect', props);
+    console.log('in effect', props, books);
   });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const keywords = joinWords(searchTerms);
+    const author = joinWords(searchAuthor);
+    const options = {
+      method: "GET",
+      url: `https://www.googleapis.com/books/v1/volumes?q=${keywords}+inauthor:${author}`,
+      params: {
+        key: process.env.GOOGLE_BOOKS_KEY,
+        country: "US",
+      },
+    };
+
+    let results, error;
+
+    try {
+      results = await axios.request(options);
+    } catch(e) {
+      error = e;
+    }
+    
+    if (!error) {
+      setBooks(results.data.items);
+    } else {
+      console.error(error);
+    }
+  }
 
   return (
     <div>
-      <p>Hello</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search-terms">Search Terms</label>
+        <input
+          id="search-terms"
+          value={searchTerms}
+          onChange={(e) => setSearchTerms(e.target.value)}
+        />
+        <label htmlFor="author">Author</label>
+        <input
+          id="author"
+          value={searchAuthor}
+          onChange={(e) => setSearchAuthor(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+      {books && renderResults()}
     </div>
-  )
-}
-
-export async function getStaticProps() {
-  const options = {
-    method: "GET",
-    url: "https://www.googleapis.com/books/v1/volumes?q=sound+inauthor:falkner",
-    params: {
-      key: process.env.GOOGLE_BOOKS_KEY,
-      country: "US",
-    },
-  };
-
-  let data;
-  try {
-    const resp = await axios.request(options);
-    data = resp.data;
-    console.log(resp.data);
-  } catch (e) {
-    console.error(e)
-  }
-  // console.log("in get static props", resp.data);
-
-  return {
-    props: {
-      results: data || 'none'
-    },
-  };
+  );
 }
